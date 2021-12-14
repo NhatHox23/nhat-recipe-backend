70d197034f4c3c6f5d2d590867432056b2af99df
NhatHo <nhat.ho@go2joy.vn>
2021-12-14 14:03:02 +0700

    init custom super user

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+
+
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **kwargs):
+        """Creates and saves a new user"""
+        if "@" not in email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+
+        # using=self._db for support multiple database
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        """Create and save a superuser"""
+        user = self.model(email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
