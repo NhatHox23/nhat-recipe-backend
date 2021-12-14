7508027278a1522dfb4632a8a33d0b585d3aaae9
NhatHo <nhat.ho@go2joy.vn>
2021-12-14 15:23:11 +0700

    init admin edit page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -22,7 +22,14 @@ class AdminSiteTests(TestCase):
 
     def test_users_listed(self):
         """Test that users are listed on user page"""
-        url = reverse('admin:core_user-changelist')
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
